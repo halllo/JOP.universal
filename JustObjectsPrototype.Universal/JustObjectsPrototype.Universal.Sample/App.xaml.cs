30bbf696f6fe17cbe0e9ca4da89cdfd7ce1cf27f
Manuel Naujoks <mnaujoks@gmail.com>
2016-07-16 21:54:12 +0200

    better readme

--- JustObjectsPrototype.Universal/JustObjectsPrototype.Universal.Sample/App.xaml.cs
+++ JustObjectsPrototype.Universal/JustObjectsPrototype.Universal.Sample/App.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
 using Windows.ApplicationModel.Activation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -18,29 +16,14 @@ namespace JustObjectsPrototype.Universal.Sample
 		protected override void OnLaunched(LaunchActivatedEventArgs e)
 		{
 			var objects = new ObservableCollection<object>
-			{
-				new Akte { Name = "Akte 1", Datum = DateTime.Now },
-				new Akte { Name = "Akte 2" },
-				new Akte { Name = "Akte 3" },
-				new Kunde {
-					Vorname = "Manuel",
-					Nachname = "Naujoks",
-					Freunde = new List<Kunde> {
-						new Kunde { Vorname = "Tester1", Nachname ="Test1"},
-						new Kunde { Vorname = "Tester2", Nachname ="Test2"},
-					}
-				},
-				new Kunde { Vorname = "Max", Nachname = "Musterman" },
-				new Kunde { Vorname = "King", Nachname = "Kong" },
-				new Dokument { Inhalt = "dokument 1" },
-				new Dokument { Inhalt = "dokument 2" },
-			};
+	{
+		new Akte { Name = "Erstes Projekt", Datum = DateTime.Now },
+		new Akte { Name = "Große Akte" },
+		new Akte { Name = "Sonstiges" },
+		new Kunde { Vorname = "Manuel", Nachname = "Naujoks"},
+	};
 
-			Show.Prototype(With.These(objects)
-				.OnChanged<Akte>(a => a.ID++)
-				.OnChanged<Kunde>(k => k.Geändert = true)
-				.OnChanged<Dokument>(d => d.Geändert = true)
-				);
+			Show.Prototype(With.These(objects));
 		}
 	}
 
@@ -78,123 +61,47 @@ namespace JustObjectsPrototype.Universal.Sample
 	[JOP.Icon(Symbol.Folder)]
 	public class Akte
 	{
-		public Akte()
-		{
-		}
-
-		public int ID { get; set; }
-
 		public string Name { get; set; }
 		public Aktenstatus Status { get; set; }
 		public Kunde Mandant { get; set; }
 		public DateTime Datum { get; set; }
 
-		[JOP.Icon(Symbol.Highlight)]
-		public void Highlighten()
-		{
-			Name += "!";
-		}
-
 		[JOP.Icon(Symbol.Document)]
-		public Dokument Schreiben(string inhalt)
+		public Dokument Rechnug_Schreiben(string inhalt)
 		{
 			return new Dokument { Adressat = Mandant, Inhalt = inhalt };
 		}
 
-		[JOP.Icon(Symbol.Delete)]
-		public void Alle_Dokumente_Löschen(ObservableCollection<Dokument> dokumente)
-		{
-			dokumente.Clear();
-		}
-
-		[JOP.Icon(Symbol.Delete)]
-		public void Erstes_Dokumente_Löschen(ObservableCollection<Dokument> dokumente)
-		{
-			dokumente.Remove(dokumente.First());
-		}
-
-		[JOP.Icon(Symbol.Delete)]
-		public void Alle_Akten_Löschen(ObservableCollection<Akte> akten)
-		{
-			akten.Clear();
-		}
-
-		[JOP.Icon(Symbol.Delete)]
-		public static void Alle_Löschen(ObservableCollection<Akte> akten)
-		{
-			akten.Clear();
-		}
-
-		[JOP.Icon(Symbol.Highlight)]
-		public static void Highlight_Second(ObservableCollection<Akte> akten)
-		{
-			akten[1].Highlighten();
-		}
-
 		[JOP.Icon(Symbol.Add)]
-		public static void Neu_Erzeugen(ObservableCollection<Akte> akten, int wieviele)
+		public static void Neu(ObservableCollection<Akte> akten)
 		{
-			for (int i = 0; i < wieviele; i++)
-			{
-				akten.Add(new Akte { Name = "Neue Akte " + (i + 1), Datum = DateTime.Now });
-			}
+			akten.Add(new Akte { Name = "Neue Akte " + (akten.Count + 1), Datum = DateTime.Now });
+		}
+
+		[JOP.Icon(Symbol.Delete)]
+		public void Löschen(ObservableCollection<Akte> akten)
+		{
+			akten.Remove(this);
 		}
 	}
 
 	[JOP.Icon(Symbol.Contact)]
 	public class Kunde
 	{
-		[JOP.Editor(@readonly: true)]
-		public bool Geändert { get; set; }
-
 		public string Vorname { get; set; }
 		public string Nachname { get; set; }
-
-		public List<Kunde> Freunde { get; set; }
-		public string Freunde_Info { get { return string.Join(", ", Freunde?.Select(f => f != null ? f.ToString() : "<NULL>") ?? new List<string>()); } }
-
-		public List<string> Spitznamen { get; set; }
-		public string Spitznamen_Info { get { return string.Join(", ", Spitznamen ?? new List<string>()); } }
-
-		public List<int> Ints { get; set; }
-		public string Ints_Info { get { return string.Join(", ", Ints ?? new List<int>()); } }
 
 		public override string ToString()
 		{
 			return (Vorname + " " + Nachname).Trim();
-		}
-
-		public static void Alle_Ungeändert(ObservableCollection<Kunde> kunden)
-		{
-			foreach (var kunde in kunden)
-			{
-				kunde.Geändert = false;
-			}
-		}
-
-		[JOP.Icon(Symbol.AddFriend)]
-		public void Neuer_Freund(Kunde freund)
-		{
-			if (Freunde == null)
-			{
-				Freunde = new List<Kunde>();
-			}
-
-			Freunde.Add(freund);
 		}
 	}
 
 	[JOP.Icon(Symbol.Document)]
 	public class Dokument
 	{
-		[JOP.Editor(@readonly: true)]
-		public bool Geändert { get; set; }
-
 		public Kunde Adressat { get; set; }
 		public string Inhalt { get; set; }
-
-		[JOP.Editor(hide: true)]
-		public string Ersteller { get; set; }
 
 		[JOP.Icon(Symbol.Remove)]
 		public void Löschen(ObservableCollection<Dokument> dokumente)
--- README.md
+++ README.md
@@ -1,27 +1,85 @@
 JOP.universal - Just Objects Prototype for Windows Universal Platform
 =====================================================================
-Intuitive super fast prototyping by using skills you already know. Create only your domain objects and get a prototype shell for free.
+Intuitive super-fast prototyping with skills you already have. Create only your domain objects and get a prototype shell for free.
 
 [![Build status](https://ci.appveyor.com/api/projects/status/tg27yvopapssdquc?svg=true)](https://ci.appveyor.com/project/halllo/jop-universal)
 [![Version](https://img.shields.io/nuget/v/JOP.universal.svg)](https://www.nuget.org/packages/JOP.universal/)
 
 How To Use
 ----------
-TODO
+Just create a new Blank App (Universal Windows) and add POCO classes that implement your domain, like in the example below.
+```csharp
+public enum Aktenstatus { Potenziell, Angenommen, Abgelehnt }
 
+[JOP.Icon(Symbol.Folder)]
+public class Akte
+{
+	public string Name { get; set; }
+	public Aktenstatus Status { get; set; }
+	public Kunde Mandant { get; set; }
+	public DateTime Datum { get; set; }
+
+	[JOP.Icon(Symbol.Document)]
+	public Dokument Rechnug_Schreiben(string inhalt)
+	{
+		return new Dokument { Adressat = Mandant, Inhalt = inhalt };
+	}
+
+	[JOP.Icon(Symbol.Add)]
+	public static void Neu(ObservableCollection<Akte> akten)
+	{
+		akten.Add(new Akte { Name = "Neue Akte " + (akten.Count + 1), Datum = DateTime.Now });
+	}
+
+	[JOP.Icon(Symbol.Delete)]
+	public void Löschen(ObservableCollection<Akte> akten)
+	{
+		akten.Remove(this);
+	}
+}
+
+[JOP.Icon(Symbol.Contact)]
+public class Kunde
+{
+	public string Vorname { get; set; }
+	public string Nachname { get; set; }
+
+	public override string ToString()
+	{
+		return (Vorname + " " + Nachname).Trim();
+	}
+}
+
+[JOP.Icon(Symbol.Document)]
+public class Dokument
+{
+	public Kunde Adressat { get; set; }
+	public string Inhalt { get; set; }
+
+	[JOP.Icon(Symbol.Remove)]
+	public void Löschen(ObservableCollection<Dokument> dokumente)
+	{
+		dokumente.Remove(this);
+	}
+}
+```
+Now that you modelled your prototype domain, "Install-Package JOP.universal" and show the prototype UI using a fluent and natural language like API in the App.xaml.cs.OnLaunched method.
 ```csharp
 protected override void OnLaunched(LaunchActivatedEventArgs e)
 {
 	var objects = new ObservableCollection<object>
 	{
-		new Kunde { Vorname = "Manuel", Nachname = "Naujoks" }
+		new Kunde { Vorname = "Manuel", Nachname = "Naujoks"},
+
+		new Akte { Name = "Erstes Projekt", Datum = DateTime.Now },
+		new Akte { Name = "Große Akte" },
+		new Akte { Name = "Sonstiges" },
 	};
 
-	Show.Prototype(With.These(objects)
-		.AndViewOf<Akte>()
-		.OnChanged<Akte>(a => { a.ID++; })
-		);
+	Show.Prototype(With.These(objects));
 }
 ```
+This gets you an UI like in the screenshot below, where you can 'play' with your object model by creating and deleting instances and invoke methods of your types.
+![Screenshot](https://raw.githubusercontent.com/halllo/JOP.universal/master/screenshot.png)
 
 Happy prototyping!
