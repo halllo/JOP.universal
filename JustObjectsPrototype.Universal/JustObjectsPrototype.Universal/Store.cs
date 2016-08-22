9bbd45f4b0bac6fc9c447f594177278c06967204
Manuel Naujoks <mnaujoks@gmail.com>
2016-08-22 21:57:43 +0200

    easier storing of objects (just Strings or DataContractSerializer)

--- JustObjectsPrototype.Universal/JustObjectsPrototype.Universal/Store.cs
+++ JustObjectsPrototype.Universal/JustObjectsPrototype.Universal/Store.cs
@@ -4,14 +4,100 @@ using System.IO;
 using System.Runtime.Serialization;
 using System.Threading.Tasks;
 using Windows.Storage;
+using Windows.Storage.Streams;
 
 namespace JustObjectsPrototype.Universal
 {
-	public class Store<T>
+	public class Store
+	{
+		string foldername;
+
+		public Store(string foldername)
+		{
+			this.foldername = foldername;
+		}
+
+		protected async Task<StorageFolder> GetFolder()
+		{
+			var localFolder = ApplicationData.Current.LocalFolder;
+			var folderForType = await localFolder.CreateFolderAsync(foldername, CreationCollisionOption.OpenIfExists);
+			return folderForType;
+		}
+
+		protected async Task WriteFile(string filename, CreationCollisionOption options, Func<IRandomAccessStream, Task> writeAction)
+		{
+			var folder = await GetFolder();
+			var file = await folder.CreateFileAsync(filename, options);
+			using (var openedFile = await file.OpenAsync(FileAccessMode.ReadWrite))
+			{
+				await writeAction(openedFile);
+			}
+		}
+
+		protected async Task ReadAll(Func<IRandomAccessStream, Task> readAction)
+		{
+			var folder = await GetFolder();
+			var files = await folder.GetFilesAsync();
+			foreach (var file in files)
+			{
+				using (var openedFile = await file.OpenAsync(FileAccessMode.Read))
+				{
+					await readAction(openedFile);
+				}
+			}
+		}
+
+
+		public async Task SaveOrUpdate(string filename, string filecontent, CreationCollisionOption options = CreationCollisionOption.ReplaceExisting)
+		{
+			await WriteFile(filename, options, async openedFile =>
+			{
+				using (var streamWriter = new StreamWriter(openedFile.AsStreamForWrite()))
+				{
+					await streamWriter.WriteAsync(filecontent);
+					await streamWriter.FlushAsync();
+				}
+			});
+		}
+
+		public async Task<List<string>> All()
+		{
+			var result = new List<string>();
+			await ReadAll(async openedFile =>
+			{
+				using (var streamReader = new StreamReader(openedFile.AsStreamForRead()))
+				{
+					var filecontent = await streamReader.ReadToEndAsync();
+					result.Add(filecontent);
+				}
+			});
+			return result;
+		}
+
+		public async Task Delete(string filename)
+		{
+			var folder = await GetFolder();
+			var file = await folder.GetFileAsync(filename);
+			await file.DeleteAsync();
+		}
+
+		public async Task DeleteAll()
+		{
+			var folder = await GetFolder();
+			var files = await folder.GetFilesAsync();
+			foreach (var file in files)
+			{
+				await file.DeleteAsync();
+			}
+		}
+	}
+
+
+	public class Store<T> : Store
 	{
 		Func<T, string> filenameSelector;
 
-		public Store(Func<T, string> filenameSelector)
+		public Store(Func<T, string> filenameSelector) : base(typeof(T).FullName)
 		{
 			this.filenameSelector = filenameSelector;
 		}
@@ -23,59 +109,30 @@ namespace JustObjectsPrototype.Universal
 
 		public async Task SaveOrUpdate(T t, CreationCollisionOption options = CreationCollisionOption.ReplaceExisting)
 		{
-			var folderForType = await GetFolder();
-
-			var file = await folderForType.CreateFileAsync(filenameSelector(t), options);
-			using (var openedFile = await file.OpenAsync(FileAccessMode.ReadWrite))
+			await WriteFile(filenameSelector(t), options, async openedFile =>
 			{
 				var serializer = new DataContractSerializer(typeof(T));
 				serializer.WriteObject(openedFile.AsStreamForWrite(), t);
 				await openedFile.FlushAsync();
-			}
+			});
 		}
 
 		public async Task Delete(T t)
 		{
-			var folderForType = await GetFolder();
-
-			var file = await folderForType.GetFileAsync(filenameSelector(t));
-			await file.DeleteAsync();
+			await Delete(filenameSelector(t));
 		}
 
-		public async Task DeleteAll()
+		public new async Task<List<T>> All()
 		{
-			var folderForType = await GetFolder();
-
-			var files = await folderForType.GetFilesAsync();
-			foreach (var file in files)
-			{
-				await file.DeleteAsync();
-			}
-		}
-
-		public async Task<List<T>> All()
-		{
-			var folderForType = await GetFolder();
-
-			var files = await folderForType.GetFilesAsync();
 			var result = new List<T>();
 			var serializer = new DataContractSerializer(typeof(T));
-			foreach (var file in files)
+			await ReadAll(async openedFile =>
 			{
-				using (var openedFile = await file.OpenAsync(FileAccessMode.Read))
-				{
-					var readObject = serializer.ReadObject(openedFile.AsStreamForRead());
-					result.Add((T)readObject);
-				}
-			}
+				await Task.CompletedTask;
+				var readObject = serializer.ReadObject(openedFile.AsStreamForRead());
+				result.Add((T)readObject);
+			});
 			return result;
-		}
-
-		private static async Task<StorageFolder> GetFolder()
-		{
-			var localFolder = ApplicationData.Current.LocalFolder;
-			var folderForType = await localFolder.CreateFolderAsync(typeof(T).FullName, CreationCollisionOption.OpenIfExists);
-			return folderForType;
 		}
 	}
 }
